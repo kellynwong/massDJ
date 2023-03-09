4bb4ccf4abf1f8362aa1fbe3295dad38e6ceb657
kellyn <kellynwong@hotmail.com>
2023-03-09 23:08:01 +0800

    add start

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://urchin-app-ubp78.ondigitalocean.app",
         changeOrigin: true,
         secure: false,
       },
